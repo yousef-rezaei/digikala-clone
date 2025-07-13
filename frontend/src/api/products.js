e8a99b0c0de1cb5d74ba44f233595da4ca5774e6
yousef <yousef.rezaei9@gmail.com>
2025-07-13 10:16:12 +0200

    change API_BASE_URL for running on production

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,22 +1,27 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:4000/products';
+// const API_BASE_URL = 'http://localhost:4000/products';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
+export async function getProducts() {
+  const res = await fetch(`${API_BASE_URL}/products`);
+  return res.json();
+}
 export const getAllProducts = () => {
-  return axios.get(API_BASE);
+  return axios.get(API_BASE_URL);
 };
 
 
 export const createProduct = (data) =>
-  axios.post(API_BASE, data, {
+  axios.post(API_BASE_URL, data, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
 
 export const updateProduct = (id, data) =>
-  axios.put(`${API_BASE}/${id}`, data, {
+  axios.put(`${API_BASE_URL}/${id}`, data, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
 
 export const deleteProduct = (id) => {
-  return axios.delete(`${API_BASE}/${id}`);
+  return axios.delete(`${API_BASE_URL}/${id}`);
 };
