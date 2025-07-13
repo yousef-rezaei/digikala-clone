2e12b5cd9d8dee161a73a0ad30a51ca2ba53fee0
yousef <yousef.rezaei9@gmail.com>
2025-07-13 09:38:04 +0200

    Install @nestjs/config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,13 +6,11 @@ import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: process.env.NODE_ENV === 'production' ? 'postgres' : 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
